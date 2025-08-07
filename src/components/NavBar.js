8ee3057f6e4ee045f14bb6b6153fe51f42aae6a2
Roshan Pal <roshanpal18172@gmail.com>
2025-08-07 15:47:45 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,21 +2,21 @@ import props from 'prop-types';
 import { Link } from 'react-router-dom';
 function NavBar(props){
     return(
-        <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-dark `}>
+        <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-dark`}>
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">{props.appName}</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className="nav-link active" href="/" to="/About">About</Link>
-                    </li>
-                </ul>
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link active" to="/About">About</Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
             <div className="form-check form-switch mx-4">
@@ -25,7 +25,7 @@ function NavBar(props){
             </div>
             <Link className="navbar-brand" to="/">
                 <img src="text-editor-icon-4.jpg" alt="Logo" width="35" height="35" className="d-inline-block align-text-top" />
-             </Link>
+            </Link>
         </nav>
     );
 }
